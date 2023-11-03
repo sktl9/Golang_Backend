d3b69d58ce2bdda7953640594d2cc816f67f6d41
sktl9 <sktl.gg@mail.ru>
2023-11-04 01:02:53 +0300

    first commit

---
+++ index.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_PORT := os.Getenv("DB_PORT")
+
+	connStr := "user=" + DB_USER + " dbname=" + DB_NAME + " password=" + DB_PASSWORD + " host=" + DB_HOST + " port=" + DB_PORT + " sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
